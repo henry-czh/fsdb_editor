b506cc208d358aaa7b15d62ebbb9c53313775624
zogodo <7427829008@qq.com>
2024-05-28 19:02:21 +0800

    test select OK

--- zgd_test/main.cpp
+++ zgd_test/main.cpp
@@ -1,19 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sqlite3.h>
 
-int main(int argc, char* argv[])
+static int callback(void *data, int argc, char **argv, char **azColName)
+{
+    int i;
+    fprintf(stderr, "%s: ", (const char *)data);
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+int main(int argc, char *argv[])
 {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
+    char *sql;
+    const char *data = "Callback function called";
 
+    /* Open database */
     rc = sqlite3_open("../fsdb_writer/chi_analyzer.sqlite", &db);
-
-    if( rc ){
+    if (rc)
+    {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        return 1;
-    }else{
+        exit(0);
+    }
+    else
+    {
         fprintf(stderr, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    sql = "SELECT * from reqFlit";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, (void *)data, &zErrMsg);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else
+    {
+        fprintf(stdout, "Operation done successfully\n");
     }
     sqlite3_close(db);
     return 0;
